b54ec19088798e5563a6691f020ad4154781dd1f
YevhenOsoba <jekaosoba818@gmail.com>
2023-03-10 11:51:40 +0100

    yevhen router p1

--- cv/src/common/Router.js
+++ cv/src/common/Router.js
@@ -9,6 +9,7 @@ import User from "../pages/User/User";
 import Login from "../pages/Login/Login";
 import Admin from "../pages/Admin/Admin";
 import Andriy from "../pages/Andriy/Andriy";
+import Yevhen from "../pages/Yevhen/Yevhen";
 
 export const router = createBrowserRouter([
     {
@@ -30,6 +31,10 @@ export const router = createBrowserRouter([
             {
                 path:AppRoutes.Andriy,
                 element:<Andriy/>
+            },
+            {
+                path: AppRoutes.YEVHEN,
+                element: <Yevhen />
             }
         ]
     },
