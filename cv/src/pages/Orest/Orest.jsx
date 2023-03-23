1f7cf4900e9c9b646b101aa4ecbb8a52c9b24cad
Orest <blyakhar76@gmail.com>
2023-03-23 21:05:28 +0200

    Lifting state up

--- cv/src/pages/Orest/Orest.jsx
+++ cv/src/pages/Orest/Orest.jsx
@@ -1,8 +1,10 @@
 import styles from './Orest.module.scss'
+import ContainerComp from "./LiftingStateUp/ContainerComp"
 const Orest = () => {
     return(
         <div>
             <h1 className={styles.title}>Orest</h1>
+            <ContainerComp/>
         </div>
     )
 }
