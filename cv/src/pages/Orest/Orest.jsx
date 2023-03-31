1cf9c906e9d558b3d6eccb39372de275a68a6718
Orest <blyakhar76@gmail.com>
2023-03-31 16:44:40 +0300

    form validation done

--- cv/src/pages/Orest/Orest.jsx
+++ cv/src/pages/Orest/Orest.jsx
@@ -1,10 +1,12 @@
 import styles from './Orest.module.scss'
 import ContainerComp from "./LiftingStateUp/ContainerComp"
+import OrestForm from './Form/OrestForm'
 const Orest = () => {
     return(
         <div>
             <h1 className={styles.title}>Orest</h1>
             <ContainerComp/>
+            <OrestForm/>
         </div>
     )
 }
