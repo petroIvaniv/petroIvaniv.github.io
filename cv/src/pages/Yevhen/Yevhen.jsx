346208faba96c890ce203e99f61453a81007be25
OsobaYevhen <osobayevhen@gmail.com>
2023-06-21 20:15:01 +0200

    Axios pagination

--- cv/src/pages/Yevhen/Yevhen.jsx
+++ cv/src/pages/Yevhen/Yevhen.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import MainComp from "./components/MainComp";
 import YevhenStyles from "./YevhenStyles.module.scss";
 import ReactForm from "./components/ReactForm";
+import AxiosPagination from "./components/AxiosPagination";
+import { YevhenUser } from "./components/User/User";
 
 const Yevhen = () => {
     return (
     <div className={YevhenStyles.container}>
         {/* <h1>Hello, my name is Yevhen</h1>
         <MainComp /> */}
-        <ReactForm />
+        {/* <ReactForm /> */}
+        <AxiosPagination Component={YevhenUser}/>
     </div>
     )
 }
